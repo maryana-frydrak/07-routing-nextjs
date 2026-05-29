71c40ce6013f95b95ddce00c02447fb96af6ad93
maryana-frydrak <maryanafrydrak@gmail.com>
2026-05-30 01:13:06 +0300

    add modal

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,13 +1,19 @@
 interface FilterLayoutProps {
   children: React.ReactNode;
   sidebar: React.ReactNode;
+  modal: React.ReactNode;
 }
 
-export default function FilterLayout({ children, sidebar }: FilterLayoutProps) {
+export default function FilterLayout({
+  children,
+  modal,
+  sidebar,
+}: FilterLayoutProps) {
   return (
     <div style={{ display: "flex" }}>
       <aside>{sidebar}</aside>
       <main>{children}</main>
+      {modal}
     </div>
   );
 }
