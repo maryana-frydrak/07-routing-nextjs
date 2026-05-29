ca53f793c7ddcc5a5cab6fb4c7048f27f6f04279
maryana-frydrak <maryanafrydrak@gmail.com>
2026-05-30 00:44:50 +0300

    fix sidebar

--- app/notes/filter/@sidebar/page.tsx
+++ app/notes/filter/@sidebar/page.tsx
@@ -1,1 +1,13 @@
 import Link from "next/link";
+import css from "./SidebarNotes.module.css";
+
+export default function SidebarPage() {
+  return (
+    <ul className={css.menuList}>
+      <li className={css.menuItem}>
+        <Link href="/notes/filter/all">All notes</Link>
+      </li>
+      {}
+    </ul>
+  );
+}
